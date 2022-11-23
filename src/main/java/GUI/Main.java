0c2f8437802bdf4deba1757bce8ed21f9521d6fe
Davide Panarella <davidepanarella.ts@gmail.com>
2022-11-23 18:24:17 +0100

    Refactoring

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,31 +1,18 @@
 package GUI;
 
-import Heckmeck.Tile;
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    private Stage stage;
     @Override
     public void start(Stage stage) {
-        this.stage = stage;
+        //this.stage = stage;
         BorderPane borderPane = new BorderPane();
         Tiles tiles = new Tiles();
         borderPane.setCenter(tiles.getTilePane());
-        //String javaVersion = System.getProperty("java.version");
-        //String javafxVersion = System.getProperty("javafx.version");
-        //Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        //Scene scene = new Scene(new StackPane(l), 640, 480);
-        //Parent root = new VBox();
-
-        //Tiles tiles = new Tiles();
 
         Scene root = new Scene(borderPane, 600, 250);
         stage.setTitle("Heckmeck");
--- src/main/java/GUI/Tiles.java
+++ src/main/java/GUI/Tiles.java
@@ -1,58 +1,38 @@
 package GUI;
 
-import Heckmeck.Tile;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.TilePane;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
-import static javafx.scene.input.KeyCode.U;
 
 public class Tiles extends TilePane{
 
-    //public Stage stage;
     private TilePane tilePane;
-    //private Scene scene;
-    private Button[] buttons;
+    private Button[] tileButtons;
     private final String worm = "\uD83D\uDc1B";
 
-//    public static TilePane getTiles(){
-//        return tilePane;
-//    }
-
     public Tiles(){
-        //this.stage = stage;
-        buildTilesUI();
-    }
-
-    private void buildTilesUI() {
         configTilePane();
-
+        tileButtons = new Button[Heckmeck.Tiles.numberOfTiles];
         for (int tileNumber = 21; tileNumber < 21+ Heckmeck.Tiles.numberOfTiles; tileNumber++) {
             if(tileNumber < 25){
                 Button button = new Button(Integer.toString(tileNumber) + '\n' + worm);
                 button.setPrefSize(60,80);
+                tileButtons[tileNumber-21]=button;
                 tilePane.getChildren().add(button);
             }
             else if( tileNumber < 29){
                 Button button = new Button(Integer.toString(tileNumber) + '\n' + worm + worm);
                 button.setPrefSize(60,80);
+                tileButtons[tileNumber-21]=button;
                 tilePane.getChildren().add(button);
             }
             else {
                 Button button = new Button(Integer.toString(tileNumber) + '\n' + worm + worm + '\n' + worm + worm);
                 button.setPrefSize(60,80);
-                //button.setAlignment(Pos.BOTTOM_LEFT);
+                tileButtons[tileNumber-21]=button;
                 tilePane.getChildren().add(button);
             }
         }
-
-//        scene = new Scene(tilePane, 600, 250);
-//        stage.setTitle("Tiles");
-//        stage.setScene(scene);
     }
 
     public TilePane getTilePane() {
@@ -66,8 +46,4 @@ public class Tiles extends TilePane{
         tilePane.setVgap(10);
         tilePane.setPrefColumns(8);
     }
-
-    /*public void show(){
-        this.stage.show();
-    }*/
 }
