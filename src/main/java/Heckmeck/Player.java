aea100d7b274f3e931daef9f9fb47f98cf192c96
dew54 <dvdvdm96@gmail.com>
2023-12-09 15:25:28 +0100

    updt: Game now works on playerID rather than playerName

--- src/main/java/Heckmeck/Player.java
+++ src/main/java/Heckmeck/Player.java
@@ -4,20 +4,20 @@ import java.util.LinkedList;
 
 public class Player {
 
-    private final String playerName;
+    private String playerName;
     private StackOfTiles playerTiles;
-    private int playerID;
+    private final int playerID;
 
-    private Player(String playerName){
-        this.playerName = playerName;
+    private Player(int playerID){
+        this.playerID = playerID;
         this.playerTiles = StackOfTiles.generate();
     }
     public LinkedList<Tile> getPlayerTiles(){
         return playerTiles.getTileList();
     }
 
-    public static Player generatePlayer(String playerName) {
-        return new Player(playerName);
+    public static Player generatePlayer(int playerID) {
+        return new Player(playerID);
     }
 
     public String getName() {
@@ -63,8 +63,8 @@ public class Player {
         return playerID;
     }
 
-    public void setPlayerID(int playerID) {
-        this.playerID = playerID;
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
     }
 
     public int getWormScore() {
