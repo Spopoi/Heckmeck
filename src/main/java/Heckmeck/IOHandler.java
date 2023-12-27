3d185a138809938a3fb28e19fff579dc068b1115
dew54 <dvdvdm96@gmail.com>
2023-12-27 17:55:14 +0100

    update: Interface clean out

--- src/main/java/Heckmeck/IOHandler.java
+++ src/main/java/Heckmeck/IOHandler.java
@@ -6,32 +6,10 @@ public interface IOHandler {
     void showTurnBeginConfirm(Player player);
     void showWelcomeMessage();
 
-    boolean wantToPlayRemote();
-
     int chooseNumberOfPlayers();
-    /*        output.printMessage("Choose number of players between 2 and 7:");
-            while(true){
-                try{
-                    int numberOfPlayer = input.chooseNumberOfPlayers();
-                    if(Rules.validNumberOfPlayer(numberOfPlayer)) return numberOfPlayer;
-                    else throw new IllegalInput("Invalid number of player, please select a number between 2 and 7");
-                } catch (IllegalInput e) {
-                    output.printMessage(e.getMessage());
-                }
-            }
-        }*/
+
     String choosePlayerName(Player player);
-/*        output.printMessage("Insert the name for player" + playerNumber);
-        while(true) {
-            try {
-                String playerName = input.choosePlayerName();
-                if (playerName.isBlank()) throw new IllegalInput("Blank name, choose a valid a one");
-                else return playerName;
-            } catch (IllegalInput e) {
-                output.printMessage(e.getMessage());
-            }
-        }
-    }*/
+
     void showBoardTiles(BoardTiles boardTiles);
 
     void askRollDiceConfirmation(String playerName);
@@ -40,42 +18,10 @@ public interface IOHandler {
 
     boolean wantToPick(Player currentPlayer, int actualDiceScore, int availableTileNumber);
 
-    /*        output.printMessage("Actual score: " + diceScore);
-            output.showWantToPick();
-            while(true) {
-                try {
-                    return input.wantToPick();
-                } catch (IllegalInput e) {
-                    output.printMessage(e.getMessage());
-                }
-            }
-        }*/
-
     boolean wantToSteal(Player currentPlayer, Player robbedPlayer);
-
-   /*     output.showWantToSteal(robbedPlayer);
-        while(true) {
-            try {
-                return input.wantToSteal();
-            } catch (IllegalInput e) {
-                output.printMessage(e.getMessage());
-            }
-        }
-    }*/
     void showPlayerData(Player currentPlayer, Dice dice, Player[] players);
-    //void showDice(Dice dice);
+
     Die.Face chooseDie(Player currentPlayer, Dice dice);
-//        output.printMessage("Pick one unselected face");
-//        while (true) {
-//            try {
-//                return input.chooseDiceFace();
-//            } catch (IllegalInput e) {
-//                output.printMessage(e.getMessage());
-//            }
-
-//        }
-
-//    }
 
     void showBustMessage();
 
@@ -86,4 +32,5 @@ public interface IOHandler {
     String askIPToConnect();
 
     boolean wantToPlayAgain();
+
 }
