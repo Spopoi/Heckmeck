18bcbea097eae14047094b406085bc14ffe05644
/ev!n <marzio92@gmail.com>
2024-01-17 23:03:24 +0100

    update: add propertiesManager to PlayerDataPanel

--- src/main/java/GUI/Panels/PlayerDataPanel.java
+++ src/main/java/GUI/Panels/PlayerDataPanel.java
@@ -3,10 +3,12 @@ package GUI.Panels;
 import Heckmeck.Components.Dice;
 import Heckmeck.Components.Die;
 import Heckmeck.Components.Player;
+import Utils.PropertiesManager;
 
 import javax.swing.*;
 import java.awt.*;
 
+import static Heckmeck.Launcher.getPropertiesManager;
 import static Utils.GUI.IconHandler.getPlayerTileIcon;
 import static Utils.GUI.LabelHandler.*;
 
@@ -23,8 +25,11 @@ public class PlayerDataPanel extends RoundedPanel{
     private static final int DICE_PANEL_HEIGHT = 120;
     private RoundedPanel playerTile;
 
+    private final PropertiesManager propertiesManager;
+
     public PlayerDataPanel(String imagePath) {
         super(imagePath);
+        propertiesManager = getPropertiesManager();
         init();
     }
 
@@ -60,7 +65,8 @@ public class PlayerDataPanel extends RoundedPanel{
     }
 
     private void initPlayerDicePanel() {
-        JLabel chosenDiceLabel = getLabel("Chosen dice:", JLabel.LEFT);
+        String chosenDice = propertiesManager.getMessage("chosenDice");
+        JLabel chosenDiceLabel = getLabel(chosenDice, JLabel.LEFT);
         add(chosenDiceLabel);
 
         playerDicePanel = new JPanel();
@@ -78,16 +84,20 @@ public class PlayerDataPanel extends RoundedPanel{
         playerTilePanel.setAlignmentX(LEFT_ALIGNMENT);
         playerTilePanel.setOpaque(false);
 
-        JLabel lastTileLabel = getLabel("Top tile: ", JLabel.LEFT);
+        String topTile = propertiesManager.getMessage("topTile");
+        JLabel lastTileLabel = getLabel(topTile, JLabel.LEFT);
         playerTilePanel.add(lastTileLabel);
         playerTilePanel.add(Box.createRigidArea(new Dimension(PLAYERTILE_GAP, 0)));
         add(playerTilePanel);
     }
 
     public void update(Player player, Dice dice) {
-        playerName.setText("Player " + player.getName());
-        playerWormScore.setText("Worm score: " + player.getWormScore());
-        score.setText("Current score: " + dice.getScore());
+        String playerText = propertiesManager.getMessage("player");
+        playerName.setText(playerText + player.getName());
+        String wormScore = propertiesManager.getMessage("wormScore");
+        playerWormScore.setText(wormScore + player.getWormScore());
+        String currentScore = propertiesManager.getMessage("currentScore");
+        score.setText(currentScore + dice.getScore());
         updateDicePanel(dice);
         updateTilePanel(player);
 
@@ -111,7 +121,6 @@ public class PlayerDataPanel extends RoundedPanel{
         }
     }
 
-    //TODO: extract this and horizontalSeparator
     private Dimension getDimensions(int height){
         return new Dimension(0,height);
     }
